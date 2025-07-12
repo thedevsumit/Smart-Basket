93c618d33aff266c1f9ef2cca991a37c1e9ab7db
Sumit <110337910+thedevsumit@users.noreply.github.com>
2025-07-12 17:05:22 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 export const firebaseConfig = {
-  apiKey: "AIzaSyAPtgUrrJz7Ei0oNx8c2GC5ULpfPAbbAL0",
-  authDomain: "smart-basket-auth.firebaseapp.com",
-  projectId: "smart-basket-auth",
-  storageBucket: "smart-basket-auth.firebasestorage.app",
-  messagingSenderId: "630611314545",
-  appId: "1:630611314545:web:ab4f2bf5ee67fcd4e9d8d0",
-  measurementId: "G-6VVF51LZJ1"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
